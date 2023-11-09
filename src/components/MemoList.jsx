42e4556645a725965cf2adb3d49bd905f85d41f1
masyuko0222 <masyuko0222@gmail.com>
2023-11-09 21:19:34 +0900

    ♻️ memoのタイトル、idの取得はMemoRowコンポーネントが責務を負うように

--- src/components/MemoList.jsx
+++ src/components/MemoList.jsx
@@ -2,28 +2,23 @@ export default function MemoList({ allMemos, onMemoRowClick }) {
   return (
     <div className="main-container__memo-list">
       {allMemos.map((memo) => (
-        <MemoRow
-          key={memo.id}
-          id={memo.id}
-          title={memo.content.split("\n")[0].trim()}
-          onMemoRowClick={onMemoRowClick}
-        />
+        <MemoRow key={memo.id} memo={memo} onMemoRowClick={onMemoRowClick} />
       ))}
     </div>
   );
 }
 
-const MemoRow = ({ id, title, onMemoRowClick }) => {
+const MemoRow = ({ memo, onMemoRowClick }) => {
   //const color = isTarget ? "black" : "blue";
 
   return (
     <p
       className="main-container__memo-list--title"
       onClick={() => {
-        onMemoRowClick(id);
+        onMemoRowClick(memo.id);
       }}
     >
-      {title}
+      {memo.content.split("\n")[0].trim()}
     </p>
   );
 };
