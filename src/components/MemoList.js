cd5c2740bfea0e6b1ea6bff009a6c7179fec1f83
masyuko0222 <masyuko0222@gmail.com>
2023-11-17 20:38:33 +0900

    ♻️ optional chainingを使うように

--- src/components/MemoList.js
+++ src/components/MemoList.js
@@ -14,8 +14,7 @@ export default function MemoList({ allMemos, editingMemo, onMemoTitleClick }) {
 }
 
 const MemoTitle = ({ memo, onMemoTitleClick, editingMemo }) => {
-  // editingMemo can be null. So need to check existence.
-  const color = editingMemo && memo.id === editingMemo.id ? "black" : "blue";
+  const color = memo.id === editingMemo?.id ? "black" : "blue"
 
   const memoTitle = memo.content.split("\n")[0].trim();
 
