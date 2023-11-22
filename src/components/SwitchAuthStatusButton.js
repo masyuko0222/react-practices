0aca585e161ace6fd1137e2c425f50e4c9c98baf
masyuko0222 <masyuko0222@gmail.com>
2023-11-23 01:01:11 +0900

    ⚡️ ログインボタン動的に。providerからはログイン状態stateをわたすようにする。

--- src/components/SwitchAuthStatusButton.js
+++ src/components/SwitchAuthStatusButton.js
@@ -2,9 +2,14 @@ import { useContext } from "react";
 import { AuthStatusContext } from "../context/AuthStatusContext";
 
 export default function SwitchAuthStatusButton() {
-  const isLoggedIn = useContext(AuthStatusContext)
+  const { isLoggedIn, setIsLoggedIn } = useContext(AuthStatusContext)
+
+  // スコープが狭いため、短め命名
+  const handleAuthButton = () => {
+    setIsLoggedIn(!isLoggedIn)
+  }
 
   return (
-    <button className="main-container--auth-button">{isLoggedIn ? "ログアウト" : "ログイン"}</button>
+    <button className="main-container--auth-button" onClick={handleAuthButton}>{isLoggedIn ? "ログアウト" : "ログイン"}</button>
   )
 }
--- src/context/AuthStatusContext.js
+++ src/context/AuthStatusContext.js
@@ -3,7 +3,8 @@ import { createContext, useState } from "react";
 export const AuthStatusContext = createContext(false);
 
 export const AuthStatusProvider = ({ children }) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(true)
   return (
-    <AuthStatusContext.Provider value={false}>{children}</AuthStatusContext.Provider>
+    <AuthStatusContext.Provider value={{ isLoggedIn, setIsLoggedIn }}>{children}</AuthStatusContext.Provider>
   )
 }
